b18d9f5ed56bfb2cabe23c5e07ceaa29300f359b
Monica Delgado <mnca.delgado@gmail.com>
2022-07-05 11:44:24 -1100

    added a list

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function getRandomNumber(){
-  var randomNum = Math.floor((Math.random() * 100));
+function getRandomNumber() {
+  var randomNum = Math.floor(Math.random() * 100);
   document.getElementById("randomNum").innerHTML = randomNum;
 }
+
+//List of names
+let listOfNames = ["Tim", "Lawrence", "Monica"];
+
+const list = document.getElementById("names");
+console.log(list);
+
+const entry = document.createElement("li");
+console.log(entry);
+
+entry.appendChild(document.createTextNode(listOfNames));
+list.appendChild(entry);
